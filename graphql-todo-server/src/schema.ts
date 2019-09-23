04794abc5fcfe3e0d57af1df38ac0762a18be036
Gion Kunz <gion@syncrea.ch>
2019-09-23 21:54:52 +0200

    Final todo app with GraphQL schema for query and create / update mutation

--- graphql-todo-server/src/schema.ts
+++ graphql-todo-server/src/schema.ts
@@ -3,13 +3,55 @@ import * as typeDefs from './schema.graphql';
 import {makeExecutableSchema} from 'graphql-tools';
 import {GraphQLSchema} from 'graphql';
 import {IResolvers} from 'graphql-tools';
+import uuid from 'uuid/v4';
+
+export interface Todo {
+  id: string;
+  title: string;
+  done: boolean;
+}
+
+export const inMemoryDb: {todos: {[id: string]: Todo}} = {
+  todos: {
+    '1': {
+      id: '1',
+      title: 'Todo 1',
+      done: false
+    }
+  }
+}
 
 export const resolvers: IResolvers = {
   Query: {
-    helloWorld(_: void, args: void): string {
-      return `👋 Hello world! 👋`;
-    },
+    getTodos(): Todo[] {
+      return Object.values(inMemoryDb.todos);
+    }
   },
+  Mutation: {
+    addTodo(_, {title,  done = false}: Todo): Todo[] {
+      const id: string = uuid();
+      inMemoryDb.todos = {
+        ...inMemoryDb.todos,
+        [id]: {
+          id,
+          title,
+          done
+        }
+      };
+      return Object.values(inMemoryDb.todos);
+    },
+    updateTodo(_, {id, done}: {id: string, done: boolean}): Todo[] {
+      inMemoryDb.todos = {
+        ...inMemoryDb.todos,
+        [id]: {
+          ...inMemoryDb.todos[id],
+          id,
+          done
+        }
+      };
+      return Object.values(inMemoryDb.todos);
+    }
+  }
 };
 
 export const schema: GraphQLSchema = makeExecutableSchema({
